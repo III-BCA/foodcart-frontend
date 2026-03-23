37a2e8dfbfea315b3d2db41f9cb3cb345c3dacef
23suca52 <23suca52@tcarts.in>
2026-03-23 21:13:27 +0530

    azure deploy

--- src/services/MenuService.js
+++ src/services/MenuService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://foodcart-backend1-c9f0ghbgh0dgf7gw.centralindia-01.azurewebsites.net";
+const API_URL = "https://foodcart-backend1-c9f0ghbgh0dgf7gw.centralindia-01.azurewebsites.net/";
 
 export const getMenus = () => axios.get(API_URL);
 export const addMenu = (menu) => axios.post(API_URL, menu);
